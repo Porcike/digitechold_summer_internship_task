3319a14a86a702eac48c8722576394ef3503c07c
Nagy Márk <nagymark0305@gmail.com>
2025-06-10 21:36:07 +0200

    Added individual querying and search functionality to LeisureVenueController
    Individual querying is based on the LeisureVenue ID. The result will be not found if there is no matching LeisureVenue in the dataset.
    Filtering is available for all of the fields. I won't go into details here because the mechanism is very similar to the previous solutions.

--- Smurf_Village_Statistical_Office/Controllers/LeisureVenueController.cs
+++ Smurf_Village_Statistical_Office/Controllers/LeisureVenueController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Smurf_Village_Statistical_Office.Data;
+using Smurf_Village_Statistical_Office.Utils;
 
 namespace Smurf_Village_Statistical_Office.Controllers
 {
@@ -17,10 +18,28 @@ namespace Smurf_Village_Statistical_Office.Controllers
         }
 
         [HttpGet("LeisureVenues")]
-        public async Task<IActionResult> GetLeisureVenues()
+        public async Task<IActionResult> GetLeisureVenues(
+            [FromQuery] string? name,
+            [FromQuery] int? minCapacity,
+            [FromQuery] int? maxCapacity,
+            [FromQuery] string? member,
+            [FromQuery] string? brand)
         {
-            var venues = await _context.LeisureVenues
+            var isNameInvalid = string.IsNullOrEmpty(name);
+            var isMinCapacityInvalid = minCapacity == null;
+            var isMaxCapacityInvalid = maxCapacity == null;
+            var isMemberInvalid = string.IsNullOrEmpty(member);
+            var isBrandInvalid = string.IsNullOrEmpty(brand);
 
+            var brandParseWasSuccessful = Enum.TryParse(brand, true, out Brand parsedBrand);
+
+            var venues = await _context.LeisureVenues
+                .Where(v => 
+                    (isNameInvalid || EF.Functions.Like(v.Name, name)) &&
+                    (isMinCapacityInvalid || v.Capacity >= minCapacity) &&
+                    (isMaxCapacityInvalid || v.Capacity <= maxCapacity) &&
+                    (isMemberInvalid || v.Members.Any(m => EF.Functions.Like(m.Name, member))) &&
+                    (isBrandInvalid || (brandParseWasSuccessful && v.AcceptedBrand == parsedBrand)))
                 .Select(v => new
                 {
                     v.Id,
@@ -33,6 +52,25 @@ namespace Smurf_Village_Statistical_Office.Controllers
                 .ToListAsync();
 
             return Ok(venues);
+        }
+
+        [HttpGet("LeisureVenues/{id}")]
+        public async Task<IActionResult> GetLeisureVenue([FromRoute] int id)
+        {
+            var venue = await _context.LeisureVenues.
+                Where(v => v.Id == id)
+                .Select(v => new
+                {
+                    v.Id,
+                    v.Name,
+                    v.Capacity,
+                    v.AcceptedBrand,
+                    MemberIds = v.Members.Select(m => m.Id).ToList()
+                })
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+            return venue == null ? NotFound() : Ok(venue);
         }
     }
 }
